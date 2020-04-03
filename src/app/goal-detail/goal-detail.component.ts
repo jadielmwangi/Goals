b1ad5aa0de362b259d950c058325333bedb0def8
jadielmwangi <jedielmwangi@gmail.com>
2020-04-03 20:36:27 +0300

    adds initial files

---
+++ src/app/goal-detail/goal-detail.component.ts
@@ -0,0 +1,44 @@
+// import { Component, OnInit } from '@angular/core';
+// import { Goal } from '../goal';
+// @Component({
+//   selector: 'app-goal-detail',
+//   templateUrl: './goal-detail.component.html',
+//   styleUrls: ['./goal-detail.component.css']
+// })
+// export class GoalDetailComponent implements OnInit {
+//   @Input() goal: Goal;
+
+//   constructor() { }
+
+//   ngOnInit() {
+//   }
+
+import { Component, OnInit, Input,Output, EventEmitter } from '@angular/core';
+import { Goal } from '../goal';
+// import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-goal-detail',
+  templateUrl: './goal-detail.component.html',
+  styleUrls: ['./goal-detail.component.css']
+})
+export class GoalDetailComponent implements OnInit {
+
+  // @Input() goal: Goal;
+
+  @Input() goal: Goal;
+  @Output() isComplete = new EventEmitter<boolean>();
+
+  goalComplete(complete:boolean){
+    this.isComplete.emit(complete);
+  }
+
+  goalDelete(complete:boolean){
+    this.isComplete.emit(complete);
+  }
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
